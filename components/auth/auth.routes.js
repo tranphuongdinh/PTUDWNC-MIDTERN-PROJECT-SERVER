a6339ece12b11ba7cf0159017234f4e74b7e4419
ntt2k1 <boombeachbill@gmail.com>
2022-11-17 15:29:19 +0700

    Add SendEmail Service

--- components/auth/auth.routes.js
+++ components/auth/auth.routes.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { AUTH_ROUTE } from '../../constants/routes.js';
-import authenticationMiddleware from '../../middleware/auth.middleware.js';
 import { login, register } from './auth.controller.js';
+import { sendEmail } from '../../config/email/emailService.js';
 
 const router = express.Router();
 
 router.post(AUTH_ROUTE.LOGIN, login);
 router.post(AUTH_ROUTE.REGISTER, register);
-router.get('/test', authenticationMiddleware, (req, res) => {
-  res.json({ name: 'thoi' });
+router.get('/test', (req, res) => {
+  sendEmail('boombeachbill@gmail.com','xasivo3854@klblogs.com','OK', 'Yes sir')
+  res.send({status: 'Success'});
 });
 
 export default router;
